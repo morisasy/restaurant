d1fd1d7de60e70215a5c96519351cb520bd81083
morisasy <morisasy@gmail.com>
2018-04-18 15:45:55 +0300

    CSS update

--- js/main.js
+++ js/main.js
@@ -141,6 +141,7 @@ createRestaurantHTML = (restaurant) => {
 
   const image = document.createElement('img');
   image.className = 'restaurant-img';
+  image.alt = restaurant.name;
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
   li.append(image);
 
