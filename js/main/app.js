b7720d7739617c44e844722c9fa4c2821d9b0d01
morisasy <morisasy@gmail.com>
2018-04-18 16:33:55 +0300

    ServiceWorker updated

--- js/main/app.js
+++ js/main/app.js
@@ -1,10 +1,5 @@
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/sw.js')
-  .then(function() {
-    // registration worked
-    console.log('Registration succeeded.');
-  }).catch(function(error) {
-    // registration failed
-    console.log('Registration failed with ' + error);
-  });
+  navigator.serviceWorker
+           .register('./sw.js')
+           .then(function() { console.log('Service Worker Registered'); });
 }
