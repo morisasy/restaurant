d732fa3d23f58c9f7d6ff650cf753b4ec70581da
morisasy <morisasy@gmail.com>
2018-05-03 21:17:01 +0300

    Second submission

--- js/main/app.js
+++ js/main/app.js
@@ -9,19 +9,3 @@ if ('serviceWorker' in navigator) {
   });
 }
 
-/**
- 
-if (!('serviceWorker' in navigator)) {
-  console.log('Service worker not supported');
-  return;
-}
-navigator.serviceWorker.register('service-worker.js')
-      .then(function() {
-        console.log('Registered');
-      })
-      .catch(function(error) {
-        console.log('Registration failed:', error);
-      });
-
- */
-
