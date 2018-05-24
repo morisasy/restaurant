9e40f026ed4afbae5d527f0c403fdd7d6d8f55a7
morisasy <morisasy@gmail.com>
2018-05-24 23:39:51 +0300

    Adding IndexDB

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
 var CACHE_VERSION = 'restaurant-static-v1';
+const dbName = "dbRestaurant-static";
 
 var CACHE_URLS = [
           '/',
@@ -21,8 +22,40 @@ var CACHE_URLS = [
           '/img/9.jpg',
           '/img/10.jpg'
 ];
+///**
+// This is what our customer data looks like.
+const restaurantData = [
+  { id: 1, name: "Bill", age: 35, email: "bill@company.com" },
+  { id: 2, name: "Donna", age: 32, email: "donna@home.org" }
+];
+ //*/
 
+var request = indexedDB.open(dbName, 1);
+/** 
+var request = indexedDB.open('dbRestaurants', 1, function(upgradeDB) {
+  var keyValStore = upgradeDB.createObjectStore('restaurants',{keyPath: 'name'});
+      keyValStore.put('Hello', 'World');
+});
+*/
+request.onupgradeneeded = function(event) { 
+  // Save the IDBDatabase interface 
+  var db = event.target.result;
 
+  // Create an objectStore for this database
+  var objectStore = db.createObjectStore("restaurants", { keyPath: "id" });
+
+  // Each restaurant has unique id.
+  objectStore.createIndex("id", "id", { unique: true });
+
+  objectStore.transaction.oncomplete = function(event) {
+    // Store values in the newly created objectStore.
+    var transaction = db.transaction("restaurants", "readwrite");
+    var restaurantObjectStore = transaction.objectStore("restaurants");
+    restaurantData.forEach(function(restaurant) {
+      restaurantObjectStore.add(restaurant);
+    });
+  };
+};
 self.addEventListener('install', function (event) {
     event.waitUntil(
         caches.open(CACHE_VERSION)
