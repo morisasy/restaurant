3974f42998aa12add09aab252e3d5a87a4e9da02
morisasy <morisasy@gmail.com>
2018-06-04 00:21:16 +0300

    indexDB improved

--- js/lib/index.js
+++ js/lib/index.js
@@ -2,22 +2,15 @@
 //const idb = require('idb');
 let restaurantsJSON,
  request,
- db,
  objectStore,
  tx;
 
 const dbName = "dbRestaurant-static";
 const URL = `http://localhost:1337/restaurants`;
 const opt = {credentials: 'include'};
+var db;
 
 
-/*
-const dbPromise = idb.open('restaurants', 1, upgradeDB => {
-    upgradeDB.createObjectStore('keyval');
-  });
-
-
-*/
 fetch(URL,opt)
 .then(response => response.json())
 .then(json => {
@@ -29,15 +22,24 @@ fetch(URL,opt)
   console.log('There has been a problem with your fetch operation: ', error.message);
   
 });
-
   
  request = indexedDB.open(dbName, 1);
 
+ request.onerror = (event) => {
+  // Handle errors.
+};
+
+
+ request.onsuccess = (event) => {
+  // DoSomething
+  db = request.result;
+  console.log('Print open db', db);
+};
+
     
-  request.onupgradeneeded = function(e) { 
+  request.onupgradeneeded = function(event) { 
     // Save the IDBDatabase interface 
-    db = e.target.result;
-  
+    var db = event.target.result;
     // Create an objectStore for this database
     objectStore = db.createObjectStore("restaurants", { keyPath: "id" });
    // var objectStore = db.createObjectStore("restaurants");
@@ -56,10 +58,44 @@ fetch(URL,opt)
     };
   };
   
-  request.onerror = (e) => {
-    // Handle errors.
+  
+
+  let dataStore= [];
+  /*
+  let  objectStore = db.transaction("restaurants").objectStore("restaurants");
+  var tx = db.transaction('restaurants');
+  var peopleStore = tx.objectStore('restaurants');
+  var idIndex = peopleStore.index('id');
+
+
+  console.log('Retrieved from IndexDb', idIndex.getAll());
+
+  restaurants = restaurants.push(idIndex.getAll())
+
+  */
+ // objectStore = db.transaction("restaurants").objectStore("restaurants");
+ function retrieveAll(){
+  var objectStore = db.transaction("restaurants").objectStore("restaurants");
+  objectStore.openCursor().onsuccess = (event) =>{
+    let cursor = event.target.result;
+    if (cursor) {
+      dataStore.push(cursor.value);
+      cursor.continue();
+    }
+    else {
+     console.log ('All restaurants ', restaurants);
+    }
   };
 
-  request.onsuccess = (event) => {
-    // DoSomething
-  };
+ }
+ /* 
+let  index = objectStore.index("id");
+index.openKeyCursor().onsuccess = function(event) {
+  var cursor = event.target.result;
+  if (cursor) {
+    // cursor.key is a name, like "Bill", and cursor.value is the SSN.
+    // No way to directly get the rest of the stored object
+    cursor.continue();
+  }
+};
+*/
